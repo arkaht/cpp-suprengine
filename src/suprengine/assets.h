ef31a519a2a051f371c5ba4fdffe57813537f239
Guthen <114919245+arkaht@users.noreply.github.com>
2022-10-29 15:34:24 +0200

    [added] assets path getter

--- src/suprengine/assets.h
+++ src/suprengine/assets.h
@@ -16,7 +16,9 @@ namespace suprengine
 		Assets() = delete;
 
 		static void set_render_batch( RenderBatch* _render_batch ) { render_batch = _render_batch; }
-		static void set_path( std::string _path ) { path = _path; }
+		
+		static void set_path( const std::string& _path ) { path = _path; }
+		static std::string get_path() { return path; }
 
 		static Texture* get_texture( const std::string& filename );
 		static void release();
