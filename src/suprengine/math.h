06ae649632f2ea3e5fdc39f49ddff29c3f0d5bab
Guthen <114919245+arkaht@users.noreply.github.com>
2022-10-29 20:52:00 +0200

    [added] math library w/ angle utils

---
+++ src/suprengine/math.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <cmath>
+
+namespace suprengine
+{
+	constexpr double PI = 3.141592653589793;
+	constexpr double DEG2RAD = PI / 180.0;
+	constexpr double RAD2DEG = 180.0 / PI;
+}
