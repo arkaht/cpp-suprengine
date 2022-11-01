43c268faebe8eacb15d54892fba8ddb10fe8fe9c
Guthen <114919245+arkaht@users.noreply.github.com>
2022-11-01 18:03:57 +0100

    [modified] pacman anim block at frame 1

--- src/demos/pacman/entities/pacman.hpp
+++ src/demos/pacman/entities/pacman.hpp
@@ -19,7 +19,6 @@ public:
 		//  create animator
 		anim = new AnimSpriteRenderer( this, Assets::get_texture( "atlas.png" ), 16 );
 		anim->gen_frames( Rect { 0.0f, 0.0f, 48.0f, 16.0f }, Vec2 { 16.0f, 16.0f } );
-		//anim->add_frame( Rect { 16.0f, 0.0f, 16.0f, 16.0f } );
 		anim->dest = { Level::TILE_SIZE / 2.0f, Level::TILE_SIZE / 2.0f, 16.0f, 16.0f };
 	}
 
@@ -27,5 +26,11 @@ public:
 	{
 		//  play animation if moving
 		anim->is_playing = !mover->is_blocked;
+
+		//  avoid full circle image when blocked
+		if ( !anim->is_playing && anim->current_frame == 2 )
+		{
+			anim->set_current_frame( 1 );
+		}
 	}
 };
