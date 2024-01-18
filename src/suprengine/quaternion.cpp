dbdcedc4595a9796195a95410faaed5f87341bf7
Guthen <114919245+arkaht@users.noreply.github.com>
2024-01-18 16:05:20 +0100

    [fixed] Quaternion look at wouuuuuuuwuuwuwuw

--- src/suprengine/quaternion.cpp
+++ src/suprengine/quaternion.cpp
@@ -23,17 +23,19 @@ Quaternion::Quaternion( const Vec3& axis, float angle )
 
 Quaternion::Quaternion( const Vec3& angles )
 {
-	float cr = math::cos( angles.x * 0.5f );
-	float sr = math::sin( angles.x * 0.5f );
-	float cp = math::cos( angles.y * 0.5f );
-	float sp = math::sin( angles.y * 0.5f );
-	float cy = math::cos( angles.z * 0.5f );
-	float sy = math::sin( angles.z * 0.5f );
+	float sp = math::sin( angles.x * 0.5f );
+	float cp = math::cos( angles.x * 0.5f );
 
-	w = cr * cp * cy + sr * sp * sy;
-	x = sr * cp * cy - cr * sp * sy;
-	y = cr * sp * cy + sr * cp * sy;
-	z = cr * cp * sy - sr * sp * cy;
+	float sy = math::sin( angles.y * 0.5f );
+	float cy = math::cos( angles.y * 0.5f );
+
+	float sr = math::sin( angles.z * 0.5f );
+	float cr = math::cos( angles.z * 0.5f );
+
+	x =  cr * sp * sy  -  sr * cp * cy;
+	y = -cr * sp * cy  -  sr * cp * sy;
+	z =  cr * cp * sy  -  sr * sp * cy;
+	w =  cr * cp * cy  +  sr * sp * sy;
 }
 
 void Quaternion::set( float inX, float inY, float inZ, float inW )
@@ -181,62 +183,35 @@ Quaternion Quaternion::concatenate( const Quaternion& q, const Quaternion& p )
 	return retVal;
 }
 
-//  https://answers.unity.com/questions/467614/what-is-the-source-code-of-quaternionlookrotation.html
+/*
+ *  I didn't think I would say that one day but..
+ *  Thanks Unreal for your code!
+ * 
+ *  I spent so much time trying to fix a weird 90 degrees offset 
+ *  bug with the old look_at function I had (basically pointing 
+ *  targets with their up axis instead of their forward), which 
+ *  I believe was fundamentally messed up with my coordinates system.
+ * 
+ *  Therefore, as Unreal was using the same coordinates system than me,
+ *  I just looked at their code and adapted it.
+ *  (also fixing the Quaternion constructor w/ Vec3 angles)
+ * 
+ *  Happy to finally code again :)
+ */
 Quaternion Quaternion::look_at( const Vec3& forward, const Vec3& up )
 {
-	Vec3 dir = forward.normalized();
-	Vec3 axis = Vec3::cross( up, dir ).normalized();
-	Vec3 vector3 = Vec3::cross( dir, axis );
+	const Vec3 dir = forward.normalized();
 
-	float m00 = axis.x;
-	float m01 = axis.y;
-	float m02 = axis.z;
-	float m10 = vector3.x;
-	float m11 = vector3.y;
-	float m12 = vector3.z;
-	float m20 = dir.x;
-	float m21 = dir.y;
-	float m22 = dir.z;
-
-	Quaternion quaternion;
-	float num8 = m00 + m11 + m22;
-	if ( num8 > 0.0f )
-	{
-		float num = math::sqrt( num8 + 1.0f );
-		quaternion.w = num * 0.5f;
-		num = 0.5f / num;
-		quaternion.x = ( m12 - m21 ) * num;
-		quaternion.y = ( m20 - m02 ) * num;
-		quaternion.z = ( m01 - m10 ) * num;
-		return quaternion;
-	}
-	if ( ( m00 >= m11 ) && ( m00 >= m22 ) )
-	{
-		float num7 = math::sqrt( ( ( 1.0f + m00 ) - m11 ) - m22 );
-		float num4 = 0.5f / num7;
-		quaternion.x = 0.5f * num7;
-		quaternion.y = ( m01 + m10 ) * num4;
-		quaternion.z = ( m02 + m20 ) * num4;
-		quaternion.w = ( m12 - m21 ) * num4;
-		return quaternion;
-	}
-	if ( m11 > m22 )
-	{
-		float num6 = math::sqrt( ( ( 1.0f + m11 ) - m00 ) - m22 );
-		float num3 = 0.5f / num6;
-		quaternion.x = ( m10 + m01 ) * num3;
-		quaternion.y = 0.5f * num6;
-		quaternion.z = ( m21 + m12 ) * num3;
-		quaternion.w = ( m20 - m02 ) * num3;
-		return quaternion;
-	}
-	float num5 = math::sqrt( ( ( 1.0f + m22 ) - m00 ) - m11 );
-	float num2 = 0.5f / num5;
-	quaternion.x = ( m20 + m02 ) * num2;
-	quaternion.y = ( m21 + m12 ) * num2;
-	quaternion.z = 0.5f * num5;
-	quaternion.w = ( m01 - m10 ) * num2;
-	return quaternion;
+	return Quaternion(
+		Vec3 {
+			math::atan2( 
+				dir.z, 
+				math::sqrt( dir.x * dir.x + dir.y * dir.y ) 
+			),
+			math::atan2( dir.y, dir.x ),
+			0.0f
+		}
+	);
 }
 
 Quaternion Quaternion::look_at( const Vec3& origin, const Vec3& target, const Vec3& up )
