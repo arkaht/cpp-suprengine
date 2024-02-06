297ffbb9373d8b5792d26dcb910a0266c272f2da
Guthen <114919245+arkaht@users.noreply.github.com>
2024-02-06 14:40:41 +0100

    [refactor] GuidedMissile w/ LifetimeComponent

--- src/demos/puzzle/entities/guided-missile.cpp
+++ src/demos/puzzle/entities/guided-missile.cpp
@@ -18,51 +18,111 @@ GuidedMissile::GuidedMissile(
 		color
 	);
 	_model_renderer->draw_only_outline = true;
+
+	_lifetime_component = create_component<LifetimeComponent>( LIFETIME );
+	_lifetime_component->on_time_out.listen( "owner", 
+		std::bind( &GuidedMissile::kill, this ) );
 }
 
 void GuidedMissile::update_this( float dt )
 {
-	if ( auto target = _wk_target.lock() )
-	{
-		Vec3 diff = target->location - transform->location;
+	//  accelerate speeds
+	_current_move_speed = math::lerp( 
+		_current_move_speed, 
+		move_speed, 
+		dt * move_acceleration 
+	);
+	_current_rotation_speed = math::lerp(
+		_current_rotation_speed,
+		rotation_speed,
+		dt * rotation_acceleration
+	);
 
-		float distance = diff.length();
-		if ( distance <= impact_distance )
-		{
-			auto health = target->get_owner()->get_component<HealthComponent>();
-			if ( health )
-			{
-				DamageInfo info {};
-				info.attacker = _owner;
-				info.damage = damage_amount;
+	_update_target( dt );
 
-				DamageResult result = health->damage( info );
-
-				//  alert owner
-				if ( result.is_valid && _owner )
-				{
-					_owner->on_hit.invoke( result );
-				}
-			}
-			
-			printf( "boom\n" );
-			kill();
-			return;
-		}
-
-		Vec3 direction = diff * ( 1.0f / distance );
-		Quaternion look_rotation = Quaternion::look_at( direction, up_direction );
-		transform->set_rotation( 
-			Quaternion::slerp( 
-				transform->rotation,
-				look_rotation,
-				dt * rotation_speed
-			)
-		);
-	}
-
+	//  move forward
 	transform->set_location( 
 		transform->location 
-	  + transform->get_forward() * move_speed * dt 
+	  + transform->get_forward() * _current_move_speed * dt
 	);
+
+	_check_impact();
+}
+
+void GuidedMissile::_update_target( float dt )
+{
+	auto target = _wk_target.lock();
+	if ( !target ) return;
+
+	Vec3 diff = target->location - transform->location;
+
+	//  explode if close enough
+	float distance = diff.length();
+	/*if ( distance <= impact_distance )
+	{
+		auto health = target->get_owner()->get_component<HealthComponent>();
+		_damage( health );
+		return;
+	}*/
+
+	//  rotate towards target
+	Vec3 direction = diff * ( 1.0f / distance );
+	Quaternion look_rotation = Quaternion::look_at( direction, up_direction );
+	transform->set_rotation(
+		Quaternion::slerp(
+			transform->rotation,
+			look_rotation,
+			dt * _current_rotation_speed
+		)
+	);
+}
+
+void GuidedMissile::_check_impact()
+{
+	auto physics = _engine->get_physics();
+
+	//  setup raycast
+	Ray ray( 
+		transform->location, 
+		transform->get_forward(), 
+		impact_distance 
+	);
+	RayParams params {};
+	params.can_hit_from_origin = false;
+
+	//  check collisions
+	RayHit hit {};
+	if ( !physics->raycast( ray, &hit, params ) ) return;
+	
+	//  check entity has health component
+	auto entity = hit.collider->get_owner();
+	auto health = entity->get_component<HealthComponent>();
+	if ( !health ) return;
+	
+	//  damage it
+	_damage( health );
+	//printf( "hit %p (self:%p)\n", hit.collider->get_owner(), this );
+}
+
+void GuidedMissile::_damage( std::shared_ptr<HealthComponent> target )
+{
+	if ( !target ) return;
+	
+	Vec3 diff = target->transform->location - transform->location;
+
+	//  damage
+	DamageInfo info {};
+	info.attacker = _owner;
+	info.damage = damage_amount;
+	info.knockback = diff.normalized() * knockback_force;
+	DamageResult result = target->damage( info );
+
+	//  alert owner
+	if ( result.is_valid && _owner )
+	{
+		_owner->on_hit.invoke( result );
+	}
+
+	kill();
+	//printf( "boom\n" );
 }
--- src/demos/puzzle/entities/guided-missile.h
+++ src/demos/puzzle/entities/guided-missile.h
@@ -2,11 +2,14 @@
 
 #include <components/stylized-model-renderer.h>
 
+#include <suprengine/components/lifetime-component.h>
+
 namespace puzzle
 {
 	using namespace suprengine;
 
 	class Spaceship;
+	class HealthComponent;
 
 	class GuidedMissile : public Entity
 	{
@@ -21,17 +24,34 @@ namespace puzzle
 
 	public:
 		float move_speed = 150.0f;
-		float rotation_speed = 5.0f;
+		float move_acceleration = 16.0f;
+
+		float rotation_speed = 30.0f;
+		float rotation_acceleration = 2.0f;
+
 		float impact_distance = 4.0f;
-		float damage_amount = 10.0f;
-		//float knockback_force = 15.0f;
+		float damage_amount = 15.0f;
+		float knockback_force = 45.0f;
 
 		Vec3 up_direction { Vec3::up };
 
 	private:
+		void _update_target( float dt );
+		void _check_impact();
+
+		void _damage( std::shared_ptr<HealthComponent> target );
+
+	private:
+		const float LIFETIME = 3.0f;
+
+	private:
+		float _current_move_speed { 0.0f };
+		float _current_rotation_speed { 0.0f };
+
 		std::weak_ptr<Transform> _wk_target;
 		Spaceship* _owner;
 
 		std::shared_ptr<StylizedModelRenderer> _model_renderer;
+		std::shared_ptr<LifetimeComponent> _lifetime_component;
 	};
 }
