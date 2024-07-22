17a41286ddf1038181f93c06de56beb618ebbdd6
Guthen <114919245+arkaht@users.noreply.github.com>
2024-07-22 22:03:31 +0200

    [add] JSON template functions

--- src/suprengine/json.cpp
+++ src/suprengine/json.cpp
@@ -2,9 +2,9 @@
 
 using namespace suprengine;
 
-static void _add_value( 
+void json::add_value( 
 	json::document& doc, 
-	const std::string& key, json::value&& json_value 
+	const std::string& key, json::value& json_value 
 )
 {
 	auto& allocator = doc.GetAllocator();
@@ -12,41 +12,33 @@ static void _add_value(
 	doc.AddMember( json_key, json_value, allocator );
 }
 
-//  NOTE: When creating string values whose lifetime end before 
-//		  document writing, use GenericDocument::GetAllocator()
-//		  to copy the value. Otherwise, it will reference a null value.
-
-void json::add_string( 
-	json::document& doc, 
-	const std::string& key, const std::string& value 
-)
+template<>
+void json::add( document& doc, const std::string& key, std::string value )
 {
 	json::value json_value( value.c_str(), value.size(), doc.GetAllocator() );
-	_add_value( doc, key, std::move( json_value ) );
+	add_value( doc, key, json_value );
 }
 
-void json::add_color( 
-	document& doc, 
-	const std::string& key, const Color& value
-)
+template<>
+void json::add( document& doc, const std::string& key, Color value )
 {
-	std::string hex = value.to_hex();
-	json::value json_value( hex.c_str(), hex.size(), doc.GetAllocator() );
-	_add_value( doc, key, std::move( json_value ) );
+	add( doc, key, value.to_hex() );
 }
 
-void json::add_int( 
-	document& doc, 
-	const std::string& key, int value 
+template<>
+std::string json::get( 
+	const value& obj, 
+	const std::string& key, std::string default_value 
 )
 {
-	_add_value( doc, key, json::value( value ) );
+	return get<const char*>( obj, key, default_value.c_str() );
 }
 
-void json::add_float( 
-	document& doc, 
-	const std::string& key, float value 
+template <>
+Color json::get<Color>( 
+	const value& obj, 
+	const std::string& key, Color default_value 
 )
 {
-	_add_value( doc, key, json::value( value ) );
+	return Color::from_hex( json::get<const char*>( obj, key, "00000000" ) );
 }
--- src/suprengine/json.h
+++ src/suprengine/json.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <string>
 
 //  Use STL size type for rapidjson
@@ -11,14 +13,57 @@ namespace rapidjson
 #include <rapidjson/prettywriter.h>
 
 #include <suprengine/color.h>
+#include <suprengine/logger.h>
 
 namespace suprengine::json
 {
 	using document = rapidjson::Document;
 	using value = rapidjson::Value;
 
-	void add_string( document& doc, const std::string& key, const std::string& value );
-	void add_color( document& doc, const std::string& key, const Color& value );
-	void add_int( document& doc, const std::string& key, int value );
-	void add_float( document& doc, const std::string& key, float value );
+	/*
+	 * Add a JSON value to a JSON document.
+	 */
+	void add_value( document& doc, const std::string& key, value& value );
+
+	/*
+	 * Add a value to a JSON document.
+	 */
+	template <typename T>
+	void add( document& doc, const std::string& key, T value )
+	{
+		json::value json_value( value );
+		add_value( doc, key, json_value );
+	}
+	template <>
+	void add( document& doc, const std::string& key, std::string value );
+	template <>
+	void add( document& doc, const std::string& key, Color value );
+
+	/*
+	 * Return a value from a JSON object.
+	 */
+	template <typename T>
+	T get( const value& obj, const std::string& key, T default_value = T() )
+	{
+		if ( !obj.HasMember( key.c_str() ) ) 
+		{
+			Logger::error( "JSON: Couldn't get value of key '%s', return a default value.", key.c_str() );
+			return default_value;
+		}
+
+		return obj[key.c_str()].Get<T>();
+	}
+	template <>
+	std::string get( const value& obj, const std::string& key, std::string default_value );
+	template <>
+	Color get( const value& obj, const std::string& key, Color default_value );
+
+	/*
+	 * Get a value from a JSON object and fill the given variable with.
+	 */
+	template <typename T>
+	void get( const value& obj, const std::string& key, T* out, T default_value = T() )
+	{
+		*out = get( obj, key, default_value );
+	}
 }
