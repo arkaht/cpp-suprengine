de3608a2f50e3d49bcb034757f419df31e2033a0
arkaht <114919245+arkaht@users.noreply.github.com>
2024-09-12 14:16:12 +0200

    [add] ImGui extra functions for percent

--- src/suprengine/imgui.h
+++ src/suprengine/imgui.h
@@ -8,12 +8,47 @@ namespace ImGui::Extra
 	static void ColoredProgressBar(
 		float fraction,
 		ImVec4 color,
-		const ImVec2& size_arg = ImVec2( -FLT_MIN, 0 ), 
+		const ImVec2& size_arg = ImVec2( -FLT_MIN, 0 ),
 		const char* overlay = NULL
 	)
 	{
 		ImGui::PushStyleColor( ImGuiCol_PlotHistogram, color );
 		ImGui::ProgressBar( fraction, size_arg, overlay );
 		ImGui::PopStyleColor();
+	}
+
+	static bool SliderPercent(
+		const char* label,
+		float* v,
+		float v_min, float v_max,
+		const char* format = "%.0f%%"
+	)
+	{
+		float percent = *v * 100.0f;
+		if ( ImGui::SliderFloat( label,	&percent, v_min * 100.0f, v_max * 100.0f, format ) )
+		{
+			*v = percent / 100.0f;
+			return true;
+		}
+
+		return false;
+	}
+
+	static bool DragPercent(
+		const char* label,
+		float* v,
+		float v_speed,
+		float v_min, float v_max,
+		const char* format = "%.0f%%"
+	)
+	{
+		float percent = *v * 100.0f;
+		if ( ImGui::DragFloat( label, &percent, v_speed * 100.0f, v_min * 100.0f, v_max * 100.0f, format ) )
+		{
+			*v = percent / 100.0f;
+			return true;
+		}
+
+		return false;
 	}
 }
