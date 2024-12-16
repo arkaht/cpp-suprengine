31693193d3d6c8cd1d2261eace7dbf1c858991a1
arkaht <114919245+arkaht@users.noreply.github.com>
2024-12-16 12:39:29 +0100

    [refactor] inline all math functions and constants

--- src/suprengine/math.h
+++ src/suprengine/math.h
@@ -2,58 +2,138 @@
 
 #include <limits>
 #include <string>
+#include <cmath>
 
 namespace suprengine::math
 {
-	constexpr float PI = 3.141592f;
-	constexpr float DOUBLE_PI = PI * 2.0f;
-	constexpr float HALF_PI = PI / 2.0f;
-	constexpr float DEG2RAD = PI / 180.0f;
-	constexpr float RAD2DEG = 180.0f / PI;
+	inline constexpr float PI = 3.141592f;
+	inline constexpr float DOUBLE_PI = PI * 2.0f;
+	inline constexpr float HALF_PI = PI / 2.0f;
+	inline constexpr float DEG2RAD = PI / 180.0f;
+	inline constexpr float RAD2DEG = 180.0f / PI;
 
-	constexpr float PLUS_INFINITY = std::numeric_limits<float>::infinity();
-	constexpr float NEG_INFINITY = -std::numeric_limits<float>::infinity();
+	inline constexpr float PLUS_INFINITY = std::numeric_limits<float>::infinity();
+	inline constexpr float NEG_INFINITY = -std::numeric_limits<float>::infinity();
 
-	constexpr float EPSILON = 0.001f;
+	inline constexpr float EPSILON = 0.001f;
 
-	float abs( float value );
-	float sign( float value );
+	inline float abs( float value )
+	{
+		return fabs( value );
+	}
+	inline float sign( float value )
+	{
+		return value >= 0.0f ? 1.0f : -1.0f;
+	}
 	
-	float pow( float value, float exp );
-	int   pow( int value, int exp );
-	float sqrt( float value );
-	float fmod( float value, float div );
-	float modulo( float value, float div );
+	inline float pow( float value, float exp )
+	{
+		return powf( value, exp );
+	}
+	inline int   pow( int value, int exp )
+	{
+		return (int)std::pow( value, exp );
+	}
+	inline float sqrt( float value )
+	{
+		return sqrtf( value );
+	}
+	inline float fmod( float value, float div )
+	{
+		return std::fmod( value, div );
+	}
+	inline float modulo( float value, float div )
+	{
+		if ( value >= 0 ) return fmod( value, div );
+		return value - floor( value / div ) * div;
+	}
 
-	float floor( float value );
-	float ceil( float value );
-	float round( float value );
+	inline float floor( float value )
+	{
+		return std::floor( value );
+	}
+	inline float ceil( float value )
+	{
+		return std::ceil( value );
+	}
+	inline float round( float value )
+	{
+		return std::round( value );
+	}
 
-	float cos( float angle );
-	float sin( float angle );
-	float tan( float angle );
+	inline float cos( float angle )
+	{
+		return cosf( angle );
+	}
+	inline float sin( float angle )
+	{
+		return sinf( angle );
+	}
+	inline float tan( float angle )
+	{
+		return tanf( angle );
+	}
 
-	float acos( float value );
-	float atan2( float y, float x );
-	float cot( float angle );
+	inline float acos( float value )
+	{
+		return acosf( value );
+	}
+	inline float atan2( float y, float x )
+	{
+		return atan2f( y, x );
+	}
+	inline float cot( float angle )
+	{
+		return 1.0f / tan( angle );
+	}
 	
-	float approach( float current, float target, float delta );
-	float snap_to_grid( float value, float grid_size );
+	inline float approach( float current, float target, float delta )
+	{
+		delta = abs( delta );
 
-	bool near_zero( float val, float epsilon = EPSILON );
-	bool near_value( float value, float target, float epsilon = EPSILON );
+		if ( current < target )
+		{
+			return fmin( current + delta, target );
+		}
+		else if ( current > target )
+		{
+			return fmax( current - delta, target );
+		}
 
-	float normalize_angle( float ang );
-	float remap( 
-		float value, 
-		float low1, float high1, 
-		float low2, float high2 
-	);
+		return target;
+	}
+	inline float snap_to_grid( float value, float grid_size )
+	{
+		return floor( value / grid_size ) * grid_size;
+	}
+
+	inline bool near_zero( float val, float epsilon = EPSILON )
+	{
+		return abs( val ) <= epsilon;
+	}
+	inline bool near_value( float value, float target, float epsilon = EPSILON )
+	{
+		float diff = abs( value - target );
+		return diff <= epsilon;
+	}
+
+	inline float normalize_angle( float ang )
+	{
+		return modulo( ang + PI, DOUBLE_PI ) - PI;
+	}
+	inline float remap(
+		float value,
+		float low1, float high1,
+		float low2, float high2
+	)
+	{
+		return low2 + ( value - low1 ) * ( high2 - low2 ) / ( high1 - low1 );
+	}
 
 	template <typename T>
-	std::string to_hex( T value, size_t hex_length = sizeof( T ) * 2 )
+	inline std::string to_hex( T value, size_t hex_length = sizeof( T ) * 2 )
 	{
-		static const char* DIGITS = "0123456789ABCDEF";
+		constexpr const char* DIGITS = "0123456789ABCDEF";
 		std::string hex( hex_length, '0' );
 
 		int i = (int)hex_length - 1;
@@ -69,28 +149,37 @@ namespace suprengine::math
 
 		return hex;
 	}
-	uint8_t from_hex( std::string hex );
+	inline uint8_t from_hex( std::string hex )
+	{
+		//  Remove optional hexadecimal indicator
+		if ( hex[0] == '#' )
+		{
+			hex = hex.erase( 0, 1 );
+		}
+
+		return std::stoi( hex, nullptr, 16 );
+	}
 
 	template <typename T>
-	T max( const T& a, const T& b )
+	inline T max( const T& a, const T& b )
 	{
 		return a < b ? b : a;
 	}
 
 	template <typename T>
-	T min( const T& a, const T& b )
+	inline T min( const T& a, const T& b )
 	{
 		return a < b ? a : b;
 	}
 
 	template <typename T>
-	T clamp( const T& value, const T& lower, const T& upper )
+	inline T clamp( const T& value, const T& lower, const T& upper )
 	{
 		return min( upper, max( lower, value ) );
 	}
 	
 	template <typename T>
-	T lerp( T a, T b, float f )
+	inline T lerp( T a, T b, float f )
 	{
 		return a + (T)( f * ( b - a ) );
 	}
